4a194321fe7a7cb3dae6248cca096ff68a0228cd
Neen-18 <kuzonkaneki@gmail.com>
2026-01-21 19:35:46 -0700

    Create week-2 page with StudentInfo component and update Root page

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Noreen Dulay</p>
+      <p>
+        Github:
+      <Link href="https://github.com/Neen-18/cprg306-assignments.git"> neen-18/cprg306-assignments</Link>
+      </p>
+    </div>
+  )
+}
