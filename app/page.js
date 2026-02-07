b79a03a2dcff02abe5a1a9a03f29f1721a873ba4
Neen-18 <kuzonkaneki@gmail.com>
2026-02-06 19:38:08 -0700

    Added week 4 components and updated title page

--- app/page.js
+++ app/page.js
@@ -6,10 +6,9 @@ export default function RootPage() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
 
       <p>
-        <Link href="/week-2">Go to Week 2</Link>
-      </p>
-      <p>
-        <Link href="/week-3">Go to Week 3</Link>
+        <Link href="/week-2">Go to Week 2 -- </Link>
+        <Link href="/week-3">Go to Week 3 -- </Link>
+        <Link href="/week-4">Go to Week 4</Link>
       </p>      
     </main>
   );
